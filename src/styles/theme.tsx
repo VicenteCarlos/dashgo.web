2885c6b4d34509c5773d5dc47c597eff4d150841
vicentecarlos <soniamariadias2012@hotmail.com>
2023-11-19 10:51:04 -0300

    style: implements theme of mui

---
+++ src/styles/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#D63E8D",
+      contrastText: "white",
+    },
+  },
+});
+
+export { theme };
