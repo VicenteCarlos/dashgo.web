4edc97f8681d59e223067fe3807f9eb66cded811
vicentecarlos <soniamariadias2012@hotmail.com>
2023-11-19 09:59:16 -0300

    chore: start project

---
+++ src/routes/routes.tsx
@@ -0,0 +1,15 @@
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+
+const routes = [
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
+
+export { routes };
